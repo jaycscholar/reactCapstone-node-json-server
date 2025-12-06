dadf9e94f4617a9d06604fdbc6fa89e8171094c7
Jay Barrios <reyezsj@gmail.com>
2025-12-06 09:57:21 -0800

    add the server

---
+++ server.js
@@ -0,0 +1,10 @@
+// server.js (or api/index.js)
+    const jsonServer = require('json-server');
+    const server = jsonServer.create();
+    const router = jsonServer.router('./src/data/employees.json'); // Path to your JSON data
+    const middlewares = jsonServer.defaults();
+
+    server.use(middlewares);
+    server.use(router);
+
+    module.exports = server; // Export the server for Vercel
